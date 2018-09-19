febe35c645ea628afd48677513517d6ab855ab24
Bridget Kalinda <bridgetkalinda@yahoo.co.uk>
2018-09-19 09:39:56 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    puts "FizzBuzz"
+    elsif int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  else
+    puts ""
+
+
+
+  end
+end
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(3 && 5)
