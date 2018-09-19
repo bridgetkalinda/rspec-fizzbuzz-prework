6442530b91df92acbb95b50cfa0887240bee0090
Bridget Kalinda <bridgetkalinda@yahoo.co.uk>
2018-09-19 09:46:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,28 +1,17 @@
-# def fizzbuzz(int)
-#   if int % 3 == 0
-#      "Fizz"
-#   elsif int % 5 == 0
-#      "Buzz"
-#   elsif int % 3 == 0 && int % 5 == 0
-#      "FizzBuzz"
-#   else
-#     puts ""
-#
-#
-#
-#   end
-# end
-# fizzbuzz(3)
-# fizzbuzz(5)
-
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    "fizzbuzz"
-  elsif int % 3 == 0
-    "fizz"
+  if int % 3 == 0
+     "Fizz"
   elsif int % 5 == 0
-    "buzz"
+     "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+     "FizzBuzz"
   else
-    int
+    puts ""
+
+
+
   end
 end
+fizzbuzz(3)
+fizzbuzz(5)
+
